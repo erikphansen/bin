d62c7e21b1d2bc0d06dcd06ae8c82514d2bea2f7
Erik P. Hansen <eph@erikphansen.com>
2016-05-19 08:00:09 -0700

    Added fish shell syntax highlighting

--- oh-my-zsh-custom/env.zsh
+++ oh-my-zsh-custom/env.zsh
@@ -2,7 +2,9 @@
 DEFAULT_USER=erikhansen
 
 # rbenv
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+if which rbenv > /dev/null;
+  then eval "$(rbenv init -)";
+fi
 
 # autojump
 [[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
