49a9f304cae90364a2262f86c155656775e63a60
Erik P. Hansen <eph@erikphansen.com>
2016-09-12 07:45:31 -0700

    Added a function to clear the Finder icon cache.

--- omf-config/functions/functions.fish
+++ omf-config/functions/functions.fish
@@ -36,11 +36,16 @@ end
 function refreshfinder
     sudo find / -name .DS_Store -delete; killall Finder
 end
+function flushiconcache -d 'Fixes incorrect Finder icons'
+    # from <http://furbo.org/2015/01/19/clearing-the-icon-services-cache-in-yosemite/>
+    sudo find /private/var/folders/ -name com.apple.dock.iconcache -exec rm {} \;
+    and sudo find /private/var/folders/ -name com.apple.iconservices -exec rm -rf {} \;
+    and sudo rm -rf /Library/Caches/com.apple.iconservices.store
+end
 
 # apps
 function e
-    # subl $argv
-    atom -n $argv
+    atom $argv
 end
 function e.
     e .
