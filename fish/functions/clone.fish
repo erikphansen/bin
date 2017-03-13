d6ebfe1b38c0b039f42c1051cf80279febdcf838
Erik P. Hansen <eph@erikphansen.com>
2017-03-13 09:41:07 -0700

    Improve `clone` function
    - fails more better if you don't pass in a source repo to clone from
    - better support for custom folder names

--- fish/functions/clone.fish
+++ fish/functions/clone.fish
@@ -1,7 +1,16 @@
 function clone -d 'clone repo and attempt to `cd` into its new directory'
-  set newdir (string split '/' $argv)
-  set newdir (string split '.' $newdir[2])
-  set newdir $newdir[1]
+  if [ (count $argv) -lt 1 ]
+    echo 'You must pass in repo address to clone from'
+    return
+  end
+  if [ (count $argv) -gt 1 ]
+    set newdir $argv[2]
+  else
+    # Example git repo address: git@github.com:erikphansen/bin.git
+    set newdir (string split '/' $argv[1])
+    set newdir (string split '.' $newdir[2])
+    set newdir $newdir[1]
+  end
   git clone $argv
   and cd $newdir
 end
