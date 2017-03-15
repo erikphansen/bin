e50b07742c470d3b1c359fe100c8f2c287dbe972
Erik P. Hansen <eph@erikphansen.com>
2017-03-14 19:45:50 -0700

    Improve the `pr` function
    - No need to enter Vim just to reuse the last commit message as the PR
    message
    - The URL of the PR is copied to the clipboard

--- fish/functions/pr.fish
+++ fish/functions/pr.fish
@@ -1,8 +1,11 @@
-# alias pr "git push; and git pull-request -b develop -o"
-function pr
+function pr -d 'Quickly make a PR of the current branch into `develop` or the branch of your choosing'
+  # Reuse the last commit message as the PR message
+  # TODO: this does not work well with multi-line commit messages...
+  # ...it fails when doing `-m $msg` below
+  set msg (git log -1 --pretty=%B)
   set targetbranch 'develop'
   if test -n "$argv"
     set targetbranch $argv[1]
   end
-  git push; and git pull-request -b $targetbranch -o
+  git push; and git pull-request -b $targetbranch -m $msg | pbcopy
 end
