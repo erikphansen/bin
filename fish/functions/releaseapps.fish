e66fedcf3a282f3762b349731fcd941b110cbc61
Erik P. Hansen <eph@erikphansen.com>
2017-05-05 13:58:19 -0700

    solid first draft of Bash scripts

--- fish/functions/releaseapps.fish
+++ fish/functions/releaseapps.fish
@@ -11,9 +11,6 @@ function releaseapps -d "Quickly make release branches, push them to github, cre
     set dirs $argv
   end
   set currentDir (pwd)
-  # collect all the PR links that the `git pull-request` command spits out
-  # this will be copied to the clipboard after making all the PRs
-  set links
   for dir in $dirs
     cd $project_dir/$dir
     git checkout develop
@@ -21,14 +18,15 @@ function releaseapps -d "Quickly make release branches, push them to github, cre
     set timestamp (date +"%Y%m%d%H%M%S")
     git checkout -b release_$timestamp
     git push
-    set pr_url (git pull-request -b master -m "Release $timestamp")
-    set links "$links$pr_url"\n
+    hub pull-request -b master -m "Release $timestamp" | pbcopy
     # delete the temp release branch
     git checkout develop
     git branch -D release_$timestamp
-    echo 'You can find your PR at: $pr_url'
+    echo ''
+    echo 'SUCCESS!'
+    echo 'You can find your PR at:' (pbpaste)
     echo '(That URL has been copied to your clipboard, as well)'
+    echo ''
   end
-  echo $links | pbcopy
   cd $currentDir
 end
