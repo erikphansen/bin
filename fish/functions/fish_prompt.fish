22f2ceacba5717257f9e386a1fef07707b1675c9
Erik P. Hansen <eph@erikphansen.com>
2017-05-19 09:56:59 -0700

    small color tweak to prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -28,12 +28,10 @@ function fish_prompt
 
   # Configure __fish_git_prompt
   set -g __fish_git_prompt_char_stateseparator ' '
-  # set -g __fish_git_prompt_color 5fdfff
-  # set -g __fish_git_prompt_color_flags df5f00
-  set -g __fish_git_prompt_color magenta
+  set -g __fish_git_prompt_color cyan
   set -g __fish_git_prompt_color_flags red
-  set -g __fish_git_prompt_color_prefix brwhite
-  set -g __fish_git_prompt_color_suffix brwhite
+  set -g __fish_git_prompt_color_prefix white
+  set -g __fish_git_prompt_color_suffix white
   set -g __fish_git_prompt_showdirtystate true
   set -g __fish_git_prompt_showuntrackedfiles true
   set -g __fish_git_prompt_showstashstate true
