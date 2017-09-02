950c41fcade3b89abd6c6fadb0015634e5ece620
Erik P. Hansen <eph@erikphansen.com>
2017-09-02 13:00:20 -0700

    Remove `wcjs` function.
    Now that I know about `npm link` I don't need this awful hack.

--- fish/functions/wcjs.fish
+++
@@ -1,9 +0,0 @@
-function wcjs -d 'Builds WCJS and copies it to the given app repo'
-  set -l currentDir (pwd)
-  cd ~/womply/womply-common-js
-  gulp build
-  if count $argv > /dev/null
-  and cp -R build ~/womply/$argv/node_modules/@womply/womply-common-js/
-  end
-  cd $currentDir
-end
