171991c6d5d9749f958d786dc88ef096da71362e
Erik P. Hansen <eph@erikphansen.com>
2018-01-22 16:00:38 -0800

    Comment out the right prompt for the time being.

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,12 +1,12 @@
-function fish_right_prompt
-  set -l exit_code $status
-  if test $exit_code -ne 0
-    set_color -o red
-  else
-    set_color -o black
-  end
-  printf '%d' $exit_code
-  set_color -o black
-  printf ' < %s' (date +%H:%M:%S)
-  set_color normal
-end
+# function fish_right_prompt
+#   set -l exit_code $status
+#   if test $exit_code -ne 0
+#     set_color -o red
+#   else
+#     set_color -o black
+#   end
+#   printf '%d' $exit_code
+#   set_color -o black
+#   printf ' < %s' (date +%H:%M:%S)
+#   set_color normal
+# end
