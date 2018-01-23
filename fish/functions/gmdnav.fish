178da003d1703df14e7ea6f2f53067b9184f9e4b
Erik P. Hansen <eph@erikphansen.com>
2018-01-22 16:01:21 -0800

    Remove the `gmdnav` function since this should be done with `npm link`

--- fish/functions/gmdnav.fish
+++
@@ -1,9 +0,0 @@
-function gmdnav -d 'Builds GMD-Nav and copies it to the given app repo'
-  set -l currentDir (pwd)
-  cd ~/womply/gmd-nav
-  gulp build
-  if count $argv > /dev/null
-    and cp -R build ~/womply/$argv/node_modules/@womply/gmd-nav/
-  end
-  cd $currentDir
-end
