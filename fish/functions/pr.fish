de9301c7a2af73b37f3a3ae6dbe1fa328c259b83
Erik P. Hansen <eph@erikphansen.com>
2018-01-30 21:32:31 -0800

    starting to make my pr function better
    - some more fixes to the `pr` function

--- fish/functions/pr.fish
+++ fish/functions/pr.fish
@@ -1,13 +1,29 @@
 function pr -d 'Quickly make a PR of the current branch into `develop` or the branch of your choosing'
   # Reuse the last commit message as the PR message
-  git log -1 --pretty=%B > msg.txt
-  set targetbranch 'develop'
+  git log -1 --pretty=%B > last_commit_message.txt
+  set -l basebranch 'develop'
+  set -l headbranch (git rev-parse --abbrev-ref HEAD)
   if count $argv > /dev/null
-    set targetbranch $argv[1]
+    set basebranch $argv[1]
   end
+  echo Pushing changes in `{$headbranch}` up to remote...
   git push
-  hub pull-request -b $targetbranch -F msg.txt | pbcopy
-  command rm msg.txt
+  echo ''
+  echo Using `hub` to make a PR from `{$headbranch}` into `{$basebranch}`...
+  set pr_url (hub pull-request -b $basebranch -F last_commit_message.txt)
+  # delete the temp file using the built in `rm` not my `rm` alias
+  command rm last_commit_message.txt
+  # if the length of $pr_url is 0...
+  if test -z $pr_url
+    set_color -o red
+    echo ''
+    echo ERROR CREATING PULL REQUEST
+    set_color normal;
+     # exit this function with an error code of 1
+    return 1
+  else
+    echo $pr_url | pbcopy
+  end
   echo ''
   set_color -o green
   echo 'SUCCESS!'
