c2b5378876361157f64eb6651d61ac84652581e1
Erik P. Hansen <eph@erikphansen.com>
2018-08-18 11:21:07 -0700

    Remove Womply dir, remove redundant color declarations

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,15 @@
-# colors
-set fish_color_autosuggestion --bold black
-set fish_color_command blue
-set fish_color_comment --bold --italics black
-set fish_color_error --bold --italics red
-set fish_color_operator cyan
-set fish_color_param cyan
-set fish_color_quote --italics green
+# This file is sourced by ~/.config/fish/config.fish, so it basically acts as
+# the root startup file for a new fish session
 
-set WOMPLY_DIR "$HOME/womply"
+# colors - commented out because these seem to have been set with the -U flag
+# So in the future I could do `set -e fish_color_autosuggestion` to delete a color
+# set fish_color_autosuggestion --bold black
+# set fish_color_command blue
+# set fish_color_comment --bold --italics black
+# set fish_color_error --bold --italics red
+# set fish_color_operator cyan
+# set fish_color_param cyan
+# set fish_color_quote --italics green
 
 source ~/bin/fish/init-rbenv.fish
 source ~/bin/fish/aliases.fish
