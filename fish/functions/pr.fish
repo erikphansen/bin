4f2eee9c294b340f6990b8faacc670ee097d2cac
Erik P. Hansen <eph@erikphansen.com>
2018-11-08 06:48:19 -0800

    fix the `pr` function so it actually adds the PR template!

--- fish/functions/pr.fish
+++ fish/functions/pr.fish
@@ -1,6 +1,13 @@
 function pr -d 'Quickly make a PR of the current branch into `develop` or the branch of your choosing'
   # Reuse the last commit message as the PR message
-  git log -1 --pretty=%B > last_commit_message.txt
+  git log -1 --pretty=%B > pr_message.txt
+
+  # Add the contents of the PULL_REQUEST_TEMPLATE.md if it exists
+  if test -f PULL_REQUEST_TEMPLATE.md
+    cat PULL_REQUEST_TEMPLATE.md >> pr_message.txt
+    # TODO: open pr_message.txt in the editor and wait before continuing
+  end
+
   set -l basebranch 'develop'
   if count $argv > /dev/null
     set basebranch $argv[1]
@@ -10,25 +17,17 @@ function pr -d 'Quickly make a PR of the current branch into `develop` or the br
   git push -u
   echo ''
   echo Using `hub` to make a PR from `{$headbranch}` into `{$basebranch}`...
-  set pr_url (hub pull-request -b $basebranch -F last_commit_message.txt)
-
-
-  # Add the contents of the PULL_REQUEST_TEMPLATE.md if it exists
-  # there is an error in opening this in the editor for tweaking before posting
-  # so the PR message needs to be updated on github after creating the PR :(
-  if test -f PULL_REQUEST_TEMPLATE.md
-    cat PULL_REQUEST_TEMPLATE.md >> last_commit_message.txt
-  end
+  set pr_url (hub pull-request -b $basebranch -F pr_message.txt)
 
   # delete the temp file using the built in `rm` not my `rm` alias to keep from polluting the macOS trashcan
-  command rm last_commit_message.txt
-  # if the length of $pr_url is 0...
+  command rm pr_message.txt
+  # if the length of $pr_url is 0 there was an error making the PR
   if test -z $pr_url
     set_color -o red
     echo ''
     echo ERROR CREATING PULL REQUEST
     set_color normal;
-     # exit this function with an error code of 1
+     # exit this `pr` function with an error code of 1
     return 1
   else
     echo $pr_url | pbcopy
