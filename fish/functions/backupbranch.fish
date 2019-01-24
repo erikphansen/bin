47a3297c35420ed096157f4f53ff13e7510bf2a6
Erik P. Hansen <eph@erikphansen.com>
2019-01-24 08:14:01 -0800

    add `backupbranch` function

---
+++ fish/functions/backupbranch.fish
@@ -0,0 +1,7 @@
+function backupbranch -d 'Makes copy of current branch, adding `-backup`'
+  # TODO: exit if not in a git repo
+  set branchname (git rev-parse --abbrev-ref HEAD)
+  set timestamp (date '+%Y-%m-%d-%I-%M-%S')
+  git branch -m "$branchname-backup-$timestamp"
+  git checkout -b "$branchname"
+end
