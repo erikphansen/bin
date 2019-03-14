e8b6ed649a6db308989cc80c81e030a180858206
Erik P. Hansen <eph@erikphansen.com>
2019-03-14 11:43:20 -0700

    Fix the backupbranch command
    Now the original branch should remember its remotes... I hope.

--- fish/functions/backupbranch.fish
+++ fish/functions/backupbranch.fish
@@ -2,6 +2,6 @@ function backupbranch -d 'Makes copy of current branch, adding `-backup`'
   # TODO: exit if not in a git repo
   set branchname (git rev-parse --abbrev-ref HEAD)
   set timestamp (date '+%Y-%m-%d-%I-%M-%S')
-  git branch -m "$branchname-backup-$timestamp"
-  git checkout -b "$branchname"
+  git checkout -b "$branchname-backup-$timestamp"
+  git checkout -
 end
