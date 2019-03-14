591c93a8c12bc3d04537523ed5dc9c9e8f3ef262
Erik P. Hansen <eph@erikphansen.com>
2019-03-14 11:40:58 -0700

    Update `t` shortcut

--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -2,8 +2,8 @@ function t -d 'tree'
   if [ (count $argv) -gt 0 ]
     set depth $argv[1]
     set args $argv[2]
-    command tree -shF --du --dirsfirst -L $depth $args
+    command tree -ashF --du --dirsfirst -L $depth $args
   else
-    command tree -shF --du --dirsfirst
+    command tree -ashF --du --dirsfirst
   end
 end
