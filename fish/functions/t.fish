cc3ef8e210a61a0498fab727c2ff7455f7ae7679
Erik P. Hansen <eph@erikphansen.com>
2019-05-03 06:45:11 -0700

    `t` defaults to showing one level of files

--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -1,9 +1,9 @@
 function t -d 'tree, add trailing -h to show sizes'
   if [ (count $argv) -gt 0 ]
     set depth $argv[1]
-    set args $argv[2]
-    command tree -aF --dirsfirst -L $depth $args
+    set rest $argv[2]
+    command tree -aF --dirsfirst -L $depth $rest
   else
-    command tree -aF --dirsfirst $args
+    command tree -aF --dirsfirst -L 1
   end
 end
