17e9c9c8a11fa6e53afb12c57cf71c66d76d198c
Erik P. Hansen <eph@erikphansen.com>
2019-10-18 18:09:09 -0700

    Enhance the backupbranch function
    - exits if you aren't in a git-controlled dir
    - quiet the output when checking out git branches (ie, make the backup silently)

--- fish/functions/backupbranch.fish
+++ fish/functions/backupbranch.fish
@@ -1,7 +1,12 @@
-function backupbranch -d 'Makes copy of current branch, adding `-backup`'
-  # TODO: exit if not in a git repo
+function backupbranch -d 'Makes copy of current branch, adding `-backup-timestamp`'
+  # exit immediately if we're not in a dir under git source control
+  if ! git rev-parse --is-inside-work-tree > /dev/null 2> /dev/null
+    echo 'You must be in a git-controlled directory to use this command'
+    return 1
+  end
+
   set branchname (git rev-parse --abbrev-ref HEAD)
   set timestamp (date '+%Y-%m-%d-%I-%M-%S')
-  git checkout -b "$branchname-backup-$timestamp"
-  git checkout -
+  git checkout --quiet -b "$branchname-backup-$timestamp" > /dev/null
+  git checkout --quiet - > /dev/null
 end
