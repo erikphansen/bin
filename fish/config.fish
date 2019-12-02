21cf19c0a2c31bc2079cb22b1e4eb84a98cd208f
Erik P. Hansen <eph@erikphansen.com>
2019-12-02 10:28:27 -0800

    update fish colors in config

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,9 @@
 
 # colors - commented out because these seem to have been set with the -U flag
 # So in the future I could do `set -e fish_color_autosuggestion` to delete a color
-# set fish_color_autosuggestion --bold black
+# set fish_color_autosuggestion --bold brblack
 # set fish_color_command blue
-# set fish_color_comment --bold --italics black
+# set fish_color_comment --bold --italics brblack
 # set fish_color_error --bold --italics red
 # set fish_color_operator cyan
 # set fish_color_param cyan
