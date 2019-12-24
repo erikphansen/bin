79a2179ecbb41f4cf4a6bba13f8e9163193383e3
Erik P. Hansen <eph@erikphansen.com>
2019-12-23 20:56:34 -0800

    set fish shell colors on startup

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,32 @@
 # This file is sourced by ~/.config/fish/config.fish, so it basically acts as
 # the root startup file for a new fish session
 
-# colors - commented out because these seem to have been set with the -U flag
-# So in the future I could do `set -e fish_color_autosuggestion` to delete a color
-# set fish_color_autosuggestion --bold brblack
-# set fish_color_command blue
-# set fish_color_comment --bold --italics brblack
-# set fish_color_error --bold --italics red
-# set fish_color_operator cyan
-# set fish_color_param cyan
-# set fish_color_quote --italics green
+set -g fish_color_autosuggestion --bold brblack
+set -g fish_color_cancel -r
+set -g fish_color_command blue
+set -g fish_color_comment --bold --italics black
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error --bold --italics red
+set -g fish_color_escape bryellow --bold
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator cyan
+set -g fish_color_param cyan
+set -g fish_color_quote --italics green
+set -g fish_color_redirection brblue
+set -g fish_color_search_match bryellow --italics --background=brblack
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_status red
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
 
 # this keeps getting reset for some reason, so I'm gonna set it with each new shell
-set -U Z_CMD j
+set -g Z_CMD j
+set -g ZO_CMD jo
 
 source ~/bin/fish/private.fish
 source ~/bin/fish/init-rbenv.fish
