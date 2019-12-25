f519f709311493f0fc98e16128a9c07ef1a660e2
Erik P. Hansen <eph@erikphansen.com>
2019-12-25 07:26:58 -0800

    support setting the base branch in the `prvw` function

--- fish/functions/prvw.fish
+++ fish/functions/prvw.fish
@@ -1,10 +1,14 @@
 function prvw
+  # TODO: make it possible to pass the base branch to this prvw func
   set -l repobase (git rev-parse --show-toplevel);
-  if echo $repobase | grep 'vets-website$' > /dev/null
-    # TODO: make it possible to pass the base branch to this prvw func
-    pr master -d --labels VSA --assign erikphansen
-  else
+  if test -z (echo $repobase | grep 'vets-website$') > /dev/null
     echo 'You must be in the vets-website repo'
     return 1
+  else
+    set -l basebranch 'master'
+    if count $argv > /dev/null
+      set basebranch $argv[1];
+    end
+    pr $basebranch -d --labels VSA --assign erikphansen
   end
 end
